33e2232a88ee6cf39e3dbef0ffe68fd689f962e1
andrew2004Z <60035892+andrew2004Z@users.noreply.github.com>
2020-11-12 12:11:27 +0300

    Add files via upload

--- ft_join.py
+++ ft_join.py
@@ -8,6 +8,6 @@ def ft_len(mass):
 def ft_join(lst, sep=' '):
     a = ''
     for i in range(ft_len(lst) - 2):
-        a += str(lst[i]) + sep 
+        a += str(lst[i]) + sep
     a += str(lst[-1])
     return a
